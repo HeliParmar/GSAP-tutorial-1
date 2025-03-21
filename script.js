cca51b3571cd1d1b203ce48dab659b5a37458afc
HeliParmar <hellyparmar17@gmail.com>
2025-03-21 14:57:41 +0530

    Updated feature X

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
 gsap.to("#page2 h1", {
-  transform:"translateX(-194%)",
+  transform:"translateX(-160%)",
   scrollTrigger:{
     trigger:"#page2",
     scroller:"body",
     markers:true,
     start:"top 0%",
     end:"top -100%",
-    scrub:2,
+    scrub:0.5,
     pin:true
   }
 })
 
-var initialPath = `M 10 100 Q 250 100 490 100`
+var Path = "M 10 100 Q 250 100 490 100"
+var finalPath = "M 10 100 Q 250 100 490 100"
+
+var string = document.querySelector("#string")
+
+string.addEventListener("mousemove", function (dets) {
+  path = `M 10 100 Q ${dets.x} ${dets.y} 490 100`
+
+  gsap.to("svg path",{
+    attr: { d: path },
+    duration: 0.5,
+    ease: "power2.inOut",
+  })
+})
+
+string.addEventListener("mouseleave",function(){
+  gsap.to("svg path",{
+    attr: {d:finalPath},
+    duration: 0.5,
+    ease:"elastic.out(1,0.2)"
+  })
+})
